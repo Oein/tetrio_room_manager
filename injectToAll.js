3b6b138037fee7c8734cb2539fd8684a59dc3c4f
Oein <oein0219@gmail.com>
2023-02-19 00:05:28 +0900

    crash handler

--- injectToAll.js
+++ injectToAll.js
@@ -4,7 +4,8 @@
     if (dig.childElementCount > 0) {
       let chi = dig.children[0].innerText;
       if (chi.includes("CONNECTION ERROR")) {
-        location.reload();
+        window.ipcRenderer.invoke("data.game.crash");
+        setTimeout(location.reload, 1000);
       }
     }
   }, 1000);
